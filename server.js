ccf07d982837798788337de1356b6899ad730894
htata31 <htata31@gmail.com>
2019-10-25 19:49:15 -0500

    Commitng server changes

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.get('/', function(req, res) {
-    res.render('index.html');
+    res.render('LoginPage');
 })
 app.post('/enroll', function (req, res) {
     MongoClient.connect(url, function(err, client) {
